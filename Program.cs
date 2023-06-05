f2f1c3b0424bcb5ed886acd7919449ca981a75be
Zach5812 <zachlouk7@gmail.com>
2023-06-05 14:33:33 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// for (int i = 1; i<=255; i++)
+// {
+//     Console.WriteLine(i);
+// };
+// Console.WriteLine("==========");
+// Random rand = new Random();
+// int sum = 0;
+// int j = 0;
+// int q = 0;
+// while(j <=4)
+// {
+//     q = rand.Next(10, 20);
+//     sum += q;
+//     j++;
+// };
+//     Console.WriteLine(sum);
+
+// Console.WriteLine("==========");
+
+int i = 1;
+while(i<=100)
+{
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i % 3 == 0 )
+    {
+    Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+    Console.WriteLine("Buzz");
+    }
+    i++;
+}
